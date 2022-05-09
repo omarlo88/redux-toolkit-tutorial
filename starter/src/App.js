38baa5420bb53a63172a998f2a203472b2347458
olo <omarlo88.omar@gmail.com>
2022-05-09 13:24:23 +0200

    init

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CartContainer from './components/CartContainer';
+import Modal from './components/Modal';
 import Navbar from './components/Navbar';
+// import { calculateTotals } from './features/cartSlice';
+
 function App() {
+  /*  const { cartItems, isOpen } = useSelector((store) => ({
+    ...store.cartReducer,
+    ...store.modalReducer,
+  })); */
+
+  const { isOpen } = useSelector((store) => ({
+    ...store.modalReducer,
+  }));
+
+  /*
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]); */
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
---
+++ starter/src/components/Modal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { clerCart } from '../features/cart/cartSlice';
+import { closeModal } from '../features/modal/modalSlice';
+
+const Modal = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <aside className='modal-container'>
+      <div className='modal'>
+        <h4>remove all items from shopping cart?</h4>
+        <div className='btn-container'>
+          <button
+            className='btn confirm-btn'
+            onClick={() => {
+              dispatch(clerCart());
+              dispatch(closeModal());
+            }}
+          >
+            confirm
+          </button>
+          <button
+            className='btn clear-btn'
+            onClick={() => dispatch(closeModal())}
+          >
+            cancel
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Modal;
